2a254c2a2bc4bee470a97930e54ea8da112c75bc
dnyana <dnyaneshgawade903@gmail.com>
2021-08-10 22:27:27 +0530

    [Dnyanesh] add :creating Binary search tree

---
+++ BinarySearchTree/BinaryTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    public class BinaryTree
+    {
+        public static  Node root;
+        public BinaryTree()
+        {
+            root = null;
+        }
+        public void Insert(int i)
+        {
+            Node newNode = new Node
+            {
+                Data = i
+            };
+            if (root == null)
+                root = newNode;
+            else
+            {
+                Node current = root;
+                Node parent;
+                while (true)
+                {
+                    parent = current;
+                    if (i < current.Data)
+                    {
+                        current = current.Left;
+                        if (current == null)
+                        {
+                            parent.Left = newNode;
+                            break;
+                        }
+
+                        else
+                        {
+                            current = current.Right;
+                            if (current == null)
+                            {
+                                parent.Right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public void Display(Node root)
+        {
+            if (root != null)
+            {
+                Display(root.Left);
+                Display(root.Right);
+                Console.WriteLine(root.Data);
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.Insert(45);
+            binaryTree.Insert(1);
+            binaryTree.Insert(23);
+            binaryTree.Display(BinaryTree.root);
+
+        }
+    }
+}
